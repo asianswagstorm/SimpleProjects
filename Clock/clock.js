3eeecbf6d3a8705592c1c521df4097ea7f3b1e4e
Andy Nguyen <brobromario@hotmail.com>
2019-05-15 01:27:53 -0400

    added circular clock with media queries

--- Clock/clock.js
+++ Clock/clock.js
@@ -1,12 +1,13 @@
 /**
  * Created by Andy on 2018-01-12.
  */
+
 function clock() {
-    var FullDate = new Date();
-    var Hour = FullDate.getHours();
-    var Minute = FullDate.getMinutes();
-    var Seconds = FullDate.getSeconds();
-    var AMPM;
+    let FullDate = new Date();
+    let Hour = FullDate.getHours();
+    let Minute = FullDate.getMinutes();
+    let Seconds = FullDate.getSeconds();
+    let AMPM;
     if(Hour>=12){  AMPM= "  : PM";}
     else AMPM ="  : AM";
 
@@ -29,10 +30,34 @@ function clock() {
         Seconds = "0"+Seconds;
     }
 
+
 //.innerHTML Change the Value
     document.getElementById('hour').innerHTML = Hour;
     document.getElementById('minute').innerHTML = " : " + Minute;
     document.getElementById('second').innerHTML = " : " + Seconds;
     document.getElementById('AMPM').innerHTML = AMPM;
 }
-setInterval(clock, 100);
+
+
+const secondHand = document.querySelector('.second-hand');
+const minsHand = document.querySelector('.min-hand');
+const hourHand = document.querySelector('.hour-hand');
+function setDate() {
+  const now = new Date();
+  const seconds = now.getSeconds();
+  const secondsDegrees = ((seconds / 60) * 360) + 90;
+  secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+  const mins = now.getMinutes();
+  const minsDegrees = ((mins / 60) * 360) + ((seconds/60)*6) + 90;
+  minsHand.style.transform = `rotate(${minsDegrees}deg)`;
+  const hour = now.getHours();
+  const hourDegrees = ((hour / 12) * 360) + ((mins/60)*30) + 90;
+  hourHand.style.transform = `rotate(${hourDegrees}deg)`;
+}
+setInterval(setDate, 1000);
+setDate();
+
+  setInterval(clock, 100);
+
+
+ 
